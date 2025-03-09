9f6a008381edf83d5a05ea253979c19cf0d92778
PriscillaRenault <priscilla.renault72460@gmail.com>
2025-03-09 17:38:19 +0100

    1 variables and show in console

---
+++ src/ej/Main.java
@@ -0,0 +1,18 @@
+package ej;
+
+public class Main {
+	public static void main(String[] args) {
+		int num;
+		num = 5;
+		num = 10;
+		
+		final int immuable = 20;
+		
+		String langage = "Java";
+		String phrase = langage + " est un langage de programmation";
+		
+		System.out.println(phrase);
+		System.out.println(num);
+		System.out.println(immuable);
+	}
+}
